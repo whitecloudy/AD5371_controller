27608786b07cbeb4125ea222f2a9399d1937d4da
Whitecloudy <alex9395@naver.com>
2020-02-07 13:00:30 +0000

    Brute Force complete

--- Naive_Beamtrainer.cpp
+++ Naive_Beamtrainer.cpp
@@ -1,30 +1,14 @@
 #include "Naive_Beamtrainer.h"
 
 const std::vector<int> Naive_Beamtrainer::getNextWeight(void){
-  if(isFind){
-    float phase = (currentPhaseVector[currentAnt] + PhaseStep) % 360;
-    //if the phase is same as initial_phase, that means 
-    if(phase == initial_phase){
-      currentPhaseVector[currentAnt] = optimalPhaseVector[currentAnt];
-      currentAnt++;
-      //if currentAnt reach antNum, it means training is over
-      if(currentAnt >= antNum){
-        isTraining = false;
-        isOptimal = true;
-      }else{
-        initial_phase = currentPhaseVector[currentAnt];
-      }
-    }else{
-      currentPhaseVector[currentAnt] = phase;
-    }
+  currentPhaseCount++;
+  int step_count = currentPhaseCount;
+  if(currentPhaseCount >= (pow(MaxStepNum, (antNum-1)))){  //this means it reaches the end and none of the phase vector able to aquire respond
+    currentPhaseVector = optimalPhaseVector;
+    isTraining = false;
   }else{
-    currentPhaseCount++;
-    int step_count = currentPhaseCount;
-    for(int i = 1; i<=antNum; i++){
-      if(i >= antNum){  //this means it reaches the end and none of the phase vector able to aquire respond
-        isTraining = false;
-        break;
-      }
+ //   printf("currentPhaseCount: %d\n",currentPhaseCount);
+    for(int i = 1; i<antNum; i++){
       currentPhaseVector[i] = (step_count % MaxStepNum)*PhaseStep + initial_phase;
       step_count /= MaxStepNum;
       if(step_count == 0)
@@ -37,22 +21,15 @@ const std::vector<int> Naive_Beamtrainer::getNextWeight(void){
 const std::vector<int> Naive_Beamtrainer::getRespond(struct average_corr_data recvData){
   if(isTraining){
     //Get respond for the first time
-    if(!isFind){
-      isFind = true;
-
-      currentAnt = 1;
-      initial_phase = currentPhaseVector[currentAnt];
-    }
 
     //add it to avg_amp for averaging 
     avg_amp += std::sqrt(recvData.avg_i * recvData.avg_i + recvData.avg_q * recvData.avg_q);
+    avgCalCount++;
 
     //Check whether it is enough for averaging
     if(avgCalCount < AvgMaxCount){
-      avgCalCount++;
       return currentPhaseVector;
-    }
-    else{
+    }else{
       avg_amp /= AvgMaxCount; //calculate average amp
       if(avg_amp > maxAvgAmp){
         maxAvgAmp = avg_amp;
@@ -61,7 +38,6 @@ const std::vector<int> Naive_Beamtrainer::getRespond(struct average_corr_data re
       avgCalCount = 0;
       avg_amp = 0;
       return getNextWeight();
-
     }
   }else
     return optimalPhaseVector;
